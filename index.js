948f7beaaa08c0a33289886d34817bdbfb13139b
JoBelmar <jose@iot4life>
2022-07-07 21:36:54 -0400

    2da subida

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
 import express from 'express';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const app = express();
 const port = 9080;
 
-console.log(__dirname);
-
 app.use(express.static('public'));
 
-app.get('/hola-mundo', (req, res) => {
-    res.send('Hola Mundo');
+app.get('/generic', (req, res) => {
+    res.sendFile(`${ __dirname }/public/generic.html`);
+});
+
+app.get('/elements', (req, res) => {
+    res.sendFile(`${ __dirname }/public/elements.html`);
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(`/public/404.html`);
+    res.sendFile(`${ __dirname }/public/404.html`);
 });
 
 app.listen(port, () => {
