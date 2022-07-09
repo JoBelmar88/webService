04a62c4e192604ac1e22f9f201dcdc2ffac4c3ee
JoBelmar <jose@iot4life>
2022-07-09 13:33:18 -0400

    servidor angular

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ hbs.registerPartials( __dirname + '/views/partials');
 
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.render(`home`, {
         nombre: 'Jose Belmar',
         titulo: 'Analista Programador'
     });
-});
+}); */
 
-app.get('/generic', (req, res) => {
+/* app.get('/generic', (req, res) => {
     res.render(`generic`, {
         nombre: 'Jose Belmar',
         titulo: 'Analista Programador'
@@ -35,17 +35,10 @@ app.get('/elements', (req, res) => {
         titulo: 'Analista Programador'
     });
 });
-
-app.get('/generic', (req, res) => {
-    res.sendFile(`${ __dirname }/public/generic.html`);
-});
-
-app.get('/elements', (req, res) => {
-    res.sendFile(`${ __dirname }/public/elements.html`);
-});
+ */
 
 app.get('*', (req, res) => {
-    res.sendFile(`${ __dirname }/public/404.html`);
+    res.sendFile(`${ __dirname }/public/index.html`);
 });
 
 app.listen(port, () => {
